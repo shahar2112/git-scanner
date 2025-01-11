d8c3357787b7734c5c318b969b5dc3c896884e3e
Shahar <shahar.maimon@novideasoft.com>
2025-01-11 15:10:40 +0200

    update

--- client/src/configs/config.js
+++ client/src/configs/config.js
@@ -1,2 +1,2 @@
 //export const GRAPHQL_URI = "http://localhost:4000/scanner";
-export const GRAPHQL_URI = "https://git-scanner.onrender.com/";
+export const GRAPHQL_URI = "https://git-scanner.onrender.com/graphql";
